03dfc4c21ec05c43d90e4fe6fd4ede9036b5dd37
Raúl <raulguzmanguerrero@outlook.com>
2020-10-07 18:00:11 -0500

    Added /:name endpoint

--- index.js
+++ index.js
@@ -27,8 +27,16 @@ const app = express();
 app.get("/", (req, res, next) => {
     res.status(200);
     res.send("Bienvenido 🙂");
-})
+});
 
-app.listen(3000, () => {
+// : antes de la ruta almacena el contenido de la ruta en una variable. Básciamente funciona como un cómodin, aquí se está diciendo que cualquier ruta que tenga algo después de / llegará a esta función
+app.get("/:name", (req, res, next) => {
+    const pokemon_name = req.params.name;
+    
+    res.status(200);
+    res.send(`Están en la página de ${pokemon_name}`);
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
