869c6eb9c83b02c962aed4ef4ff79cab91a37cd5
Raúl <raulguzmanguerrero@outlook.com>
2020-10-12 17:30:19 -0500

    Added bodyparser middleware and /pokemon POST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { pokemon } = require('./pokedex.json');
+const bodyparser = require('body-parser');
 
 console.log("Hola Mundo");
 
@@ -25,12 +26,22 @@ const app = express();
     req = información sobre la petición
     res = objeto de respuesta
 */
+
+// use -> Aplicar una función a todas las peticiones que entren a la aplicación, osea middleware
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
 app.get("/", (req, res, next) => {
     return res.status(200).send("Bienvenido al Pokedéx");
 });
 
+app.post("/pokemon", (req, res, next) => {
+    return res.status(200).send(req.body)
+});
+
 // JS lee de manera lineal los métodos GET. i.e. en este orden -> / -> /all -> /:id hasta encontrar una ruta que empate con la ruta en la petición
-app.get("/pokemon/", (req, res, next) => {
+// GET de todos los elementos sea el nombre del recurso
+app.get("/pokemon", (req, res, next) => {
     return res.status(200).send(pokemon);
 });
 
@@ -52,13 +63,17 @@ app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
 
     // retorna arreglo con los elementos que cumplan con la condición del filtro
     const queryResult = pokemon.filter((p) => {
-        return (p.name.toUpperCase() == pokemon_name.toUpperCase()) ? p : null;
+        // Si x && y es verdadero, retornar y
+        return (p.name.toUpperCase() == pokemon_name.toUpperCase()) && p;
+
+        //return (p.name.toUpperCase() == pokemon_name.toUpperCase()) ? p : null;
     });
     
     // If no Pokémon is found
     (queryResult.length > 0) ? res.status(200).send(queryResult) : res.status(404).send(`Error 404: Pokémon no encontrado`);
 });
 
+// || -> Si existe PORT usar este valor, sino utilizar 3000
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
