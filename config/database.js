69bdc7778c894df75da530c14a210cb5451e20a0
Raúl <raulguzmanguerrero@outlook.com>
2020-10-14 17:51:33 -0500

    Added dB configuration

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mysql = require(mysql);
+const util = require(util);
+
+// Mulitples conexiones
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "pokemon"
+});
+
+// Hacer que todas las query regresen promesas. No more callback hell!
+pool.query = util.promesify();
+
+module.exports = pool;
